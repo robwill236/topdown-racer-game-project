479f2af125e697327d1ae090700122afbd8f513a
samueltos <samueltos@yahoo.com>
2025-03-09 21:42:05 -0600

    new

--- scenes/MainScene/main.gd
+++ scenes/MainScene/main.gd
@@ -6,6 +6,9 @@ const SCORE_MODIFIER: int = 10
 const SPEED_MODIFIER: int = 5000
 var score: int 
 var game_running: bool
+var end_zone 
+var game_over : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,6 +16,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	if game_over:
+		$Player.velocity = Vector2.ZERO
+		return
+		
 	if game_running:
 		speed = START_SPEED + score / SPEED_MODIFIER
 		
@@ -21,9 +29,9 @@ func _process(delta: float) -> void:
 		score += speed
 		show_score()
 		
-		if $Player.position == $FinishSign.position:
-			speed = 0
-			score = 0
+		#if $Player.position == $FinishSign.position:
+			#speed = 0
+			#score = 0
 		
 	else:
 		if Input.is_action_pressed("ui_accept"):
@@ -42,3 +50,14 @@ func new_game():
 
 func show_score():
 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score/SCORE_MODIFIER)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == $Player:
+		stop_game()
+
+func stop_game():
+	game_over = true
+	$Player.velocity = Vector2i.ZERO
+	speed = 0
+	print("Game Over! You reached the end.")
