d987c2a99279bb6b86f369d52a1c599d64a59f4a
robwill236 <102136325+robwill236@users.noreply.github.com>
2025-03-25 18:44:59 -0400

    [Robert W] - The enemy moves to the correct side

--- scenes/enemies/melee_enemy.gd
+++ scenes/enemies/melee_enemy.gd
@@ -5,24 +5,29 @@ const SPEED = 100.0
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var avoidance_timer: Timer = $AvoidanceTimer
 
 var _player_detection_system: DetectionSystem
+var _avoidance_vector: Vector2 = Vector2.ZERO
 
 func _ready():
 	_player_detection_system = get_player_detection_system()
-	navigation_agent_2d.target_desired_distance = 80.0
+	navigation_agent_2d.target_desired_distance = 30.0
 
 func _physics_process(delta):
 	var target_position = _player_detection_system.get_right_detector_position()
+	print(target_position)
 	navigation_agent_2d.target_position = target_position
 	
 	var next_path_position = navigation_agent_2d.get_next_path_position()
 	var direction = (next_path_position - position).normalized()
 	
 	if navigation_agent_2d.is_navigation_finished():
+		velocity = Vector2.ZERO
 		print("stop")
 	else:
-		velocity = direction * SPEED
+		var final_direction = direction + _avoidance_vector
+		velocity = final_direction.normalized() * SPEED
 	
 	move_and_slide()
 
@@ -33,3 +38,13 @@ func get_player_detection_system() -> DetectionSystem:
 
 func attack():
 	pass
+
+
+func _on_detector_body_entered(body):
+	var away_from_obstacle = (position - body.global_position).normalized()
+	var perpendicualr = away_from_obstacle.orthogonal()
+	_avoidance_vector = perpendicualr * 5.0
+	avoidance_timer.start()
+
+func _on_avoidance_timer_timeout():
+	_avoidance_vector = Vector2.ZERO
